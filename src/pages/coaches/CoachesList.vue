801957d743b48c9023574bfe329e8fdce1c0daac
Denis Yurovsky <moon.wave578@gmail.com>
2022-10-02 19:33:15 +0300

    initial

---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,121 @@
+<template>
+  <div>
+  <base-dialog :show="!!error" title="An error occurred" @close="handleError">
+    <p>
+      {{ error }}
+    </p>
+  </base-dialog>
+  <section>
+    <coach-filter @change-filter="setFilters"></coach-filter>
+  </section>
+  <section>
+    <base-card>
+      <div class="controls">
+        <base-button @click="loadCoaches(true)" mode="outline"
+          >Refresh</base-button
+        >
+        <base-button v-if="!isCoach && !loading" link to="/register"
+          >Register as Coach</base-button
+        >
+      </div>
+      <div v-if="loading">
+        <base-spinner></base-spinner>
+      </div>
+      <ul v-else-if="hasCoaches">
+        <coach-item
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :id="coach.id"
+          :first-name="coach.firstName"
+          :last-name="coach.lastName"
+          :rate="coach.hourlyRate"
+          :areas="coach.areas"
+        ></coach-item>
+      </ul>
+      <h3 v-else>No coaches found.</h3>
+    </base-card>
+  </section>
+</div>
+</template>
+
+<script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
+import CoachFilter from '../../components/coaches/CoachFilter.vue';
+
+export default {
+  components: {
+    CoachItem,
+    CoachFilter,
+  },
+  data() {
+    return {
+      loading: false,
+      error: null,
+      activeFilters: {
+        frontend: true,
+        backend: true,
+        career: true,
+      },
+    };
+  },
+  computed: {
+    isCoach() {
+      return this.$store.getters['coaches/isCoach'];
+    },
+    filteredCoaches() {
+      const coaches = this.$store.getters['coaches/coaches'];
+      return coaches.filter((coach) => {
+        if (this.activeFilters.frontend && coach.areas.includes('frontend')) {
+          return true;
+        }
+        if (this.activeFilters.backend && coach.areas.includes('backend')) {
+          return true;
+        }
+        if (this.activeFilters.career && coach.areas.includes('career')) {
+          return true;
+        }
+        return false;
+      });
+    },
+    hasCoaches() {
+      return !this.loading && this.$store.getters['coaches/hasCoaches'];
+    },
+  },
+  methods: {
+    handleError() {
+      this.error = null;
+    },
+    setFilters(updatedFilters) {
+      this.activeFilters = updatedFilters;
+    },
+    async loadCoaches(refresh = false) {
+      this.loading = true;
+      try {
+        await this.$store.dispatch('coaches/loadCoaches', {
+          forceRefresh: refresh,
+        });
+      } catch (e) {
+        this.error = e.message;
+      }
+
+      this.loading = false;
+    },
+  },
+  created() {
+    this.loadCoaches();
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
---
+++ src/pages/requests/RequestsReceived.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <base-dialog :show="!!error" title="An error occurred" @close="handleError">
+      <p>
+        {{ error }}
+      </p>
+    </base-dialog>
+    <section>
+      <base-card>
+        <header>
+          <h2>Requests Received</h2>
+        </header>
+        <base-spinner v-if="loading"></base-spinner>
+        <ul v-else-if="!loading && hasRequests">
+          <request-item
+            v-for="req in receivedRequests"
+            :key="req.id"
+            :email="req.userEmail"
+            :message="req.message"
+          ></request-item>
+        </ul>
+        <h3 v-else>You haven't received any requests yet!</h3>
+      </base-card>
+    </section>
+  </div>
+</template>
+
+<script>
+import RequestItem from '../../components/requests/RequestItem.vue';
+
+export default {
+  components: {
+    RequestItem,
+  },
+  data() {
+    return {
+      loading: false,
+      error: null,
+    };
+  },
+  computed: {
+    receivedRequests() {
+      return this.$store.getters['requests/requests'];
+    },
+    hasRequests() {
+      return this.$store.getters['requests/hasRequests'];
+    },
+  },
+  methods: {
+    async loadRequests() {
+      this.loading = true;
+      try {
+        await this.$store.dispatch('requests/fetchRequests');
+      } catch (e) {
+        this.error = e.message;
+      }
+
+      this.loading = false;
+    },
+    handleError() {
+      this.error = null;
+    },
+  },
+  created() {
+    this.loadRequests();
+  },
+};
+</script>
+
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
